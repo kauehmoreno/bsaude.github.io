c6b11e403661a46c997f19c97d47f9bce9343d45
Kauêh Moreno <kaueh@inda.band>
2021-05-22 13:49:58 -0300

    feat

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "061c0d391810a9fa881bc28ba4c4fc48",
-"index.html": "a65d5d5548f6f4daede4c7b0003228f9",
-"/": "a65d5d5548f6f4daede4c7b0003228f9",
-"main.dart.js": "bff50884cc561b59818f1fee0b1c6336",
+"index.html": "4691b9cbf2c8a738a8de06ab57afabfc",
+"/": "4691b9cbf2c8a738a8de06ab57afabfc",
+"main.dart.js": "f5c7517c4511a7690fd4180774d79fe0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
